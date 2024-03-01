a9684b941ee4d39775856b531c94fb136a795aa3
Josh Williams <jdubz@dubzland.com>
2024-02-29 18:04:00 -0600

    Update i3/i3bar/i3status configuration

--- .config/tinted-theming/hooks/base16-i3.sh
+++ .config/tinted-theming/hooks/base16-i3.sh
@@ -6,9 +6,9 @@ function die() {
 	printf "%s\n" "$*" >&2
 }
 
-# base16-i3 theme directory
-I3_THEMES_DIR="$XDG_CONFIG_HOME/tinted-theming/base16-i3/themes"
-[[ -d "${I3_THEMES_DIR}" ]] || die "i3 themes directory not found"
+# base16-i3 colors directory
+I3_COLORS_DIR="$XDG_CONFIG_HOME/tinted-theming/base16-i3/colors"
+[[ -d "${I3_COLORS_DIR}" ]] || die "i3 colors directory not found"
 
 # i3 configuration directory
 I3_CONFIG_DIR="$XDG_CONFIG_HOME/i3"
@@ -21,11 +21,11 @@ I3_CONFIG_DROPINS="$I3_CONFIG_DIR/conf.d"
 # Current base16 theme
 [[ -n "${BASE16_THEME}" ]] || die "No base16 theme set"
 
-# Grab the i3 theme file matching the current theme
-I3_THEME_FILE="${I3_THEMES_DIR}/base16-${BASE16_THEME}.config"
-[[ -r "${I3_THEME_FILE}" ]] || die "Theme file ${I3_THEME_FILE} not found."
+# Grab the i3 colors file matching the current theme
+I3_COLORS_FILE="${I3_COLORS_DIR}/base16-${BASE16_THEME}.config"
+[[ -r "${I3_COLORS_FILE}" ]] || die "Colors file ${I3_COLORS_FILE} not found."
 
-ln -sf "${I3_THEME_FILE}" "${I3_CONFIG_DIR}/conf.d/colors.conf"
+ln -sf "${I3_COLORS_FILE}" "${I3_CONFIG_DIR}/conf.d/colors.conf"
 if command -v i3-merge-config > /dev/null; then
 	i3-merge-config -f
 	if pidof i3 >/dev/null; then
